bdaf8321b4bbbcd441606b821e50c0d8b2c3a7cc
OmuleBrianFredrick <omulebrianfredrick@gmail.com>
2025-01-07 16:32:13 +0300

    Add files via upload

---
+++ triathlonResultsSystem/src/main/java/omule/triathlonresultssystem/TriathlonResultsSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package omule.triathlonresultssystem;
+
+/**
+ *
+ * @author USER
+ */
+public class TriathlonResultsSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Triathlon Results");
+    }
+}
